4ae8691da50613e8576320ddb138f237d67a8ed2
Mika Myrseth <mika@myrseth.org>
2021-03-08 23:57:08 +0100

    changes

--- CoronaDefense/BackEnd/StorageAPI.cs
+++ CoronaDefense/BackEnd/StorageAPI.cs
@@ -1,20 +1,22 @@
-// unset
-
-using System;
+// <copyright file="StorageAPI.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
 
 namespace BackEnd
 {
+  using System;
+
   /// <summary>
   /// Interacts with Google Firebase.
   /// </summary>
   internal class StorageAPI
   {
-    public bool saveGame(GameInstance gameInstance)
+    public bool SaveGame(GameInstance gameInstance)
     {
       throw new NotImplementedException("Not implemented");
     }
 
-    public GameInstance loadGame(string userId)
+    public GameInstance LoadGame(string userId)
     {
       throw new NotImplementedException("Not implemented");
     }
