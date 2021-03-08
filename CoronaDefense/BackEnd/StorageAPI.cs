808578681df145033788cfd83dcfd3e3bf7601f8
Mika Myrseth <mika@myrseth.org>
2021-03-08 23:41:13 +0100

    Add StorageAPI class

---
+++ CoronaDefense/BackEnd/StorageAPI.cs
@@ -0,0 +1,22 @@
+// unset
+
+using System;
+
+namespace BackEnd
+{
+  /// <summary>
+  /// Interacts with Google Firebase.
+  /// </summary>
+  internal class StorageAPI
+  {
+    public bool saveGame(GameInstance gameInstance)
+    {
+      throw new NotImplementedException("Not implemented");
+    }
+
+    public GameInstance loadGame(string userId)
+    {
+      throw new NotImplementedException("Not implemented");
+    }
+  }
+}
