629bc695c429af38ed271754d6cda80b1002d5a9
le0-0 <leonard.schoyen@gmail.com>
2021-03-09 00:24:09 +0100

    Added Orchestrator skeleton.

---
+++ CoronaDefense/BackEnd/Orchestrator.cs
@@ -0,0 +1,14 @@
+﻿namespace BackEnd
+{
+  /// <summary>
+  /// A class describing a back-end orchestrator that handles global client-requests not connected to <see cref="GameInstance"/>s. This includes creating new <see cref="GameInstance"/>s.
+  /// </summary>
+  internal class Orchestrator : ApiEndpoint.Observer
+  {
+    /// <inheritdoc/>
+    internal override void GetNotified()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
