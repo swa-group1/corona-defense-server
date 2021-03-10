845072a484119b1626cbc02e72931fb2729fe2c7
le0-0 <leonard.schoyen@gmail.com>
2021-03-10 23:02:48 +0100

    Skeleton methods for Router. #4

---
+++ CoronaDefense/BackEnd/Router.IReceiver.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Router.IReceiver.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+namespace BackEnd
+{
+  internal partial class Router
+  {
+    /// <summary>
+    /// Receiver to receive messages routed via a <see cref="Router"/>.
+    /// </summary>
+    internal interface IReceiver
+    {
+      /// <summary>
+      /// Notify this <see cref="IReceiver"/>.
+      /// </summary>
+      internal void Notify()
+      {
+      }
+    }
+  }
+}
--- CoronaDefense/BackEnd/Router.cs
+++ CoronaDefense/BackEnd/Router.cs
@@ -5,21 +5,30 @@
 namespace BackEnd
 {
   /// <summary>
-  /// Class with objects that route messages from <see cref="ApiEndpoint"/> to <see cref="ModelInstance"/>.
+  /// Class with objects that route messages from <see cref="ApiEndpoint"/> to <see cref="Router.IReceiver"/>s.
   /// </summary>
-  internal class Router : ApiEndpoint.IObserver
+  internal partial class Router : ApiEndpoint.IObserver
   {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Router"/> class.
-        /// </summary>
-        /// <param name="apiEndpoint"> The Api Endpoint to connect to. </param>
-        public Router(ApiEndpoint apiEndpoint)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Router"/> class.
+    /// </summary>
+    /// <param name="api"> The Api Endpoint to connect to. </param>
+    public Router(ApiEndpoint api)
     {
-      apiEndpoint.AttachObserver(this);
+      api.AttachObserver(this);
     }
 
-        /// <inheritdoc/>
-        void ApiEndpoint.IObserver.GetNotified()
+    /// <inheritdoc/>
+    void ApiEndpoint.IObserver.GetNotified()
+    {
+      throw new System.NotImplementedException();
+    }
+    
+    /// <summary>
+    /// Register <see cref="IReceiver"/> as a possible destination for messages routed through it.
+    /// </summary>
+    /// <param name="receiver">Receiver to register.</param>
+    public void Register(IReceiver receiver)
     {
       throw new System.NotImplementedException();
     }
