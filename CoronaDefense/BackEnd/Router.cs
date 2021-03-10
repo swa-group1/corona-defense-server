edb368942c2775da3f7e6e1c13c2fc1cdaf172d7
Mika Myrseth <mika@myrseth.org>
2021-03-10 22:51:46 +0100

    Skeleton communicatio between API and endpoint?
    #3

--- CoronaDefense/BackEnd/Router.cs
+++ CoronaDefense/BackEnd/Router.cs
@@ -4,13 +4,35 @@
 
 namespace BackEnd
 {
+  using System;
+  using System.Collections.Generic;
+
   /// <summary>
   /// Class with objects that route messages from <see cref="ApiEndpoint"/> to <see cref="GameInstance"/>s.
   /// </summary>
   internal class Router : ApiEndpoint.IObserver
   {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Router"/> class.
+        /// </summary>
+        /// <param name="apiEndpoint"> The Api Endpoint to connect to. </param>
+        public Router(ApiEndpoint apiEndpoint)
+    {
+      apiEndpoint.AttachObserver(this);
+    }
+
+        private static Dictionary<string, Delegate> routeMap = new Dictionary<string, Delegate>()
+    {
+      { "/placeTower/", new Action(PlaceTower) },
+    };
+
+        private static void PlaceTower()
+    {
+      throw new System.NotImplementedException();
+    }
+
     /// <inheritdoc/>
-    void ApiEndpoint.IObserver.GetNotified()
+        void ApiEndpoint.IObserver.GetNotified()
     {
       throw new System.NotImplementedException();
     }
