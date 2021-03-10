4424827113fa73252a523168ad1576c13da74df0
le0-0 <leonard.schoyen@gmail.com>
2021-03-10 22:36:53 +0100

    Broadcaster skeleton. #2

---
+++ CoronaDefense/BackEnd/Broadcaster.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Broadcaster.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+namespace BackEnd
+{
+  using BackEnd.ModelEvents;
+  using System;
+
+  /// <summary>
+  /// Broadcaster for a <see cref="ModelInstance"/>. Functionality to send game events to all connected clients.
+  /// </summary>
+  internal class Broadcaster
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Broadcaster"/> class.
+    ///
+    /// <para>TODO: Add parameters for information about clients.</para>
+    /// </summary>
+    internal Broadcaster()
+    {
+    }
+
+    /// <summary>
+    /// Broadcast event to all clients.
+    /// </summary>
+    /// <param name="modelEvent">Event to broadcast to clients.</param>
+    internal void Broadcast(IModelEvent modelEvent)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CoronaDefense/BackEnd/ModelEvents/IModelEvent.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IModelEvent.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+namespace BackEnd.ModelEvents
+{
+  /// <summary>
+  /// Interface for objects that describe a change of a <see cref="ModelInstance"/>.
+  /// </summary>
+  internal interface IModelEvent
+  {
+  }
+}
