5615194de3f6a0bcd11e655cd01698d3fdfa3f57
Mika Myrseth <mika@myrseth.org>
2021-03-13 09:44:46 +0100

    ECS/HP: Linting and add new ChangeBy method.

--- CoronaDefense/BackEnd/ECS/HP.cs
+++ CoronaDefense/BackEnd/ECS/HP.cs
@@ -4,13 +4,30 @@
 
 namespace BackEnd.ECS
 {
-  internal readonly struct HP : IComponent
+  internal readonly struct HP
   {
-    public readonly int hp;
+    /// <summary>
+    /// Int value of current HP.
+    /// </summary>
+    public readonly int Hp;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HP"/> struct.
+    /// </summary>
+    /// <param name="hp">Initial int value of HP. </param>
     public HP(int hp)
     {
-      this.hp = hp;
+      this.Hp = hp;
+    }
+
+    /// <summary>
+    /// Get a new HP based on a change in the current HP.
+    /// </summary>
+    /// <param name="hp">Change HP by this value. Use negative to decrease. </param>
+    /// <returns> New HP with updated value. </returns>
+    public HP ChangeBy(int hp)
+    {
+      return new HP(this.Hp + hp);
     }
   }
 }
