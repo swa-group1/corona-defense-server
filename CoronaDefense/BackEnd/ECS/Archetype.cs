f73183095eae2f9ec7a1d83c4e24ce749f57d5f8
le0-0 <leonard.schoyen@gmail.com>
2021-03-13 09:45:48 +0100

    Code style and documentation for Archetype.

--- CoronaDefense/BackEnd/ECS/Archetype.cs
+++ CoronaDefense/BackEnd/ECS/Archetype.cs
@@ -1,4 +1,4 @@
-// <copyright file="IComponent.cs" company="NTNU: SWA group 1 (2021)">
+// <copyright file="Archetype.cs" company="NTNU: SWA group 1 (2021)">
 // Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
 // </copyright>
 
@@ -7,23 +7,49 @@ namespace BackEnd.ECS
   using System;
   using System.Collections.Generic;
 
+  /// <summary>
+  /// Class whose instances store a set of entities with the same component types attached.
+  /// </summary>
   internal class Archetype
   {
-    public Dictionary<Type, List<IComponent[]>> chunks = new Dictionary<Type, List<IComponent[]>>();
-    public Dictionary<int, int> identifierIndex = new Dictionary<int, int>();
-    public int numberOfEntities = 0;
+    /// <summary>
+    /// Map from component types to lists with component chunks.
+    /// </summary>
+    private Dictionary<Type, List<object[]>> chunks = new Dictionary<Type, List<object[]>>();
 
+    /// <summary>
+    /// Map from entity IDs to indices in chunks.
+    /// </summary>
+    private Dictionary<int, int> identifierIndex = new Dictionary<int, int>();
+
+    /// <summary>
+    /// Number of entities currently stored in this <see cref="Archetype"/>.
+    /// </summary>
+    private int numberOfEntities = 0;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Archetype"/> class.
+    /// </summary>
+    /// <param name="componentTypes">List of component types in arbitrary order.</param>
     public Archetype(params Type[] componentTypes)
     {
       foreach (Type componentType in componentTypes)
       {
-        this.chunks[componentType] = new List<IComponent[]>();
+        this.chunks[componentType] = new List<object[]>();
       }
     }
 
-    public void AddEntity(int entity, params IComponent[] components)
+    /// <summary>
+    /// Add supplied <paramref name="entity"/> to this <see cref="Archetype"/>.
+    /// </summary>
+    /// <remarks>
+    /// Undefined behaviour will happen if the components do not match up with the component types in this <see cref="Archetype"/>. 
+    /// </remarks>
+    /// <param name="entity">Entity to add.</param>
+    /// <param name="components">Exhaustive list of components attached to the <paramref name="entity"/>.</param>
+    public void AddEntity(int entity, params object[] components)
     {
-
+      throw new NotImplementedException();
     }
   }
 }
