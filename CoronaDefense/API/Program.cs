6cce1f15e9448a496fb70415971ae780d6de66ab
le0-0 <leonard.schoyen@gmail.com>
2021-03-15 10:54:10 +0100

    Initial ASP.NET project. #10

---
+++ CoronaDefense/API/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace API
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+      return Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+          delegate(IWebHostBuilder webBuilder)
+          {
+            webBuilder.UseStartup<Startup>();
+          }
+        );
+    }
+  }
+}
