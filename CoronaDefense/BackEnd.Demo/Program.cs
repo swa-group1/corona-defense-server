c91d28e70f55285497f5f7f7bde1374e576c7887
le0-0 <leonard.schoyen@gmail.com>
2021-04-11 22:45:34 +0200

    Added Dommy client to connect to ConnectionBroker. #57

---
+++ CoronaDefense/BackEnd.Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Program.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BackEnd.Demo
+{
+  /// <summary>
+  /// Program that connects to <see cref="BackEnd"/> via network sockets.
+  /// </summary>
+  internal static class Program
+  {
+    private const int ServerPortNumber = 19001;
+
+    /// <summary>
+    /// Main code entry-point.
+    /// </summary>
+    internal static void Main()
+    {
+      IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.IPv6Loopback, ServerPortNumber);
+
+      Socket socket = new Socket(serverEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+      socket.Connect(serverEndPoint);
+      Console.WriteLine(socket.LocalEndPoint);
+
+      byte[] connectionNumberBytes = new byte[8];
+      socket.Receive(connectionNumberBytes);
+      Console.WriteLine(BitConverter.ToString(connectionNumberBytes));
+
+      if (BitConverter.IsLittleEndian)
+      {
+        Array.Reverse(connectionNumberBytes);
+      }
+
+      socket.Close();
+
+      Console.WriteLine(BitConverter.ToString(connectionNumberBytes));
+      Console.WriteLine(BitConverter.ToInt64(connectionNumberBytes));
+    }
+  }
+}
