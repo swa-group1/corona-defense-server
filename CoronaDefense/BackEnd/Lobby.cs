67ff3f8c81b96a0f450989d9bb52e8885c66852a
le0-0 <leonard.schoyen@gmail.com>
2021-04-11 15:58:27 +0200

    Added Close logic to Lobby. #54
    Close method but also "Inactivity".

--- CoronaDefense/BackEnd/Lobby.cs
+++ CoronaDefense/BackEnd/Lobby.cs
@@ -6,6 +6,7 @@ using API.Requests;
 using API.Schemas;
 using BackEnd.Router;
 using System.Collections.Generic;
+using System.Timers;
 
 namespace BackEnd
 {
@@ -14,6 +15,11 @@ namespace BackEnd
   /// </summary>
   internal class Lobby : IReceiver
   {
+    /// <summary>
+    /// Default time before a <see cref="Lobby"/> should close itself because of inactivity.
+    /// </summary>
+    private const int lobbyTimeOut = 2 * 60 * 1000;
+
     /// <summary>
     /// Gets the ID of this <see cref="Lobby"/>.
     /// </summary>
@@ -30,9 +36,47 @@ namespace BackEnd
     private string Password { get; }
 
     /// <summary>
+    /// Backing-field for <see cref="PlayerCount"/>.
+    /// </summary>
+    private int playerCount;
+
+    /// <summary>
+    /// If <see cref="playerCount"/> is currently 0, the <see cref="Timer"/> counting towards 
+    /// </summary>
+    private Timer playerCountZeroTimer = null;
+
+    /// <summary>
     /// Gets the number of players, or clients, currently connected to this <see cref="Lobby"/>.
     /// </summary>
-    public int PlayerCount { get; private set; } = 0;
+    public int PlayerCount
+    {
+      get
+      {
+        return this.playerCount;
+      }
+
+      private set
+      {
+        this.playerCount = value;
+
+        if (value == 0)
+        {
+          Timer timer = new Timer()
+          {
+            AutoReset = false,
+            Interval = lobbyTimeOut,
+          };
+          timer.Elapsed += this.OnNoPlayerTimerElapsed;
+          timer.Start();
+          this.playerCountZeroTimer = timer;
+        }
+        else
+        {
+          this.playerCountZeroTimer?.Stop();
+          this.playerCountZeroTimer = null;
+        }
+      }
+    }
 
     private List<IObserver> Observers { get; }
 
@@ -47,6 +91,8 @@ namespace BackEnd
       this.Id = router.Register(this);
       this.Name = name;
       this.Password = password;
+
+      this.PlayerCount = 0;
     }
 
     /// <inheritdoc/>
@@ -100,9 +146,28 @@ namespace BackEnd
     /// <summary>
     /// Add observer to this Lobby.
     /// </summary>
-    public void addObserver(IObserver observer)
+    public void AddObserver(IObserver observer)
     {
       this.Observers.Add(observer);
+    }
+
+    /// <summary>
+    /// Request that this <see cref="Lobby"/> closes itself.
+    /// </summary>
+    private void OnNoPlayerTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+      this.Close();
+    }
+
+    /// <summary>
+    /// Request that this <see cref="Lobby"/> closes itself.
+    /// </summary>
+    public void Close()
+    {
+      foreach (IObserver observer in this.Observers)
+      {
+        observer.OnClose(this.Id);
+      }
     }
 
     /// <summary>
