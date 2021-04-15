7eab3ffa760f3fa9df4969f447cc0a628c3f0f06
le0-0 <leonard.schoyen@gmail.com>
2021-04-16 00:58:03 +0200

    Board position component. #81

---
+++ CoronaDefense/BackEnd/Game/Component/BoardPositionComponent.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="BoardPositionComponent.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+namespace BackEnd.Game.Component
+{
+  /// <summary>
+  /// Component containing a board position.
+  /// </summary>
+  public struct BoardPositionComponent
+  {
+    /// <summary>
+    /// X coordinate of position.
+    /// </summary>
+    public int X;
+
+    /// <summary>
+    /// Y coordinate of position.
+    /// </summary>
+    public int Y;
+  }
+}
