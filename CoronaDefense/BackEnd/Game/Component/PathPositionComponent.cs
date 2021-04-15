25b50e8b2a7a4dcf6123117dac1f28dd1ea673d0
le0-0 <leonard.schoyen@gmail.com>
2021-04-16 01:01:01 +0200

    PathPosition component. #80

---
+++ CoronaDefense/BackEnd/Game/Component/PathPositionComponent.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="PathPositionComponent.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+namespace BackEnd.Game.Component
+{
+  /// <summary>
+  /// Component describing a position along the path.
+  /// </summary>
+  public struct PathPositionComponent
+  {
+    /// <summary>
+    /// Value describing how far along the path the position is located.
+    /// </summary>
+    public double LengthTraveled;
+  }
+}
