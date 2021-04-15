97249c2852843c808412f9d2a04d8f0547183e12
le0-0 <leonard.schoyen@gmail.com>
2021-04-16 00:48:30 +0200

    HP component. #79

---
+++ CoronaDefense/BackEnd/Game/Component/HealthComponent.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="HealthComponent.cs" company="NTNU: SWA group 1 (2021)">
+// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
+// </copyright>
+
+namespace BackEnd.Game.Component
+{
+  /// <summary>
+  /// Component containing a number of health points.
+  /// </summary>
+  public struct HealthComponent
+  {
+    /// <summary>
+    /// Number of health points.
+    /// </summary>
+    public int HealthPoints;
+  }
+}
