056a73971ef29e9dc953209b1fa23c0e154b4251
le0-0 <leonard.schoyen@gmail.com>
2021-04-16 20:00:27 +0200

    Stage: Removed unused code, improved documentation, added PathLength.

--- CoronaDefense/BackEnd/Game/Stage.cs
+++ CoronaDefense/BackEnd/Game/Stage.cs
@@ -10,6 +10,7 @@ namespace BackEnd.Game
 {
   // ReSharper disable ClassNeverInstantiated.Global
   // ReSharper disable CollectionNeverUpdated.Global
+  // ReSharper disable FieldCanBeMadeReadOnly.Local
   // ReSharper disable UnusedAutoPropertyAccessor.Global
   // ↑ The stage class is created with reflection, so the init setters, Tile class, and IList fields are actually in use.
 
@@ -19,17 +20,7 @@ namespace BackEnd.Game
   internal class Stage
   {
     /// <summary>
-    /// Constant with half the number of steps in one tile.
-    /// </summary>
-    private const int HalfOfStepsInTile = 10;
-
-    /// <summary>
-    /// The number of steps in one tile.
-    /// </summary>
-    private static int StepsInTile = 2 * HalfOfStepsInTile;
-
-    /// <summary>
-    /// TODO
+    /// Gets the cumulative lengths after each segment.
     /// </summary>
     private List<double> CumulativePathLengths { get; } = new List<double>();
 
@@ -67,6 +58,17 @@ namespace BackEnd.Game
     /// Gets tiles that towers can not occupy.
     /// </summary>
     public IList<Tile> BlockedTiles { get; init; }
+
+    /// <summary>
+    /// Gets total length of path.
+    /// </summary>
+    public double PathLength
+    {
+      get
+      {
+        return this.CumulativePathLengths[^2];
+      }
+    }
 
     /// <summary>
     /// Backing-field of <see cref="PathPoints"/>.
@@ -125,19 +127,20 @@ namespace BackEnd.Game
 
       // Adding edge-case logic for requesting a point after path.
       this.CumulativePathLengths.Add(double.MaxValue);
-      this.LineSegmentsX.Add(AffineLine.Constant(this.PathPoints[this.PathPoints.Count - 1].X));
-      this.LineSegmentsY.Add(AffineLine.Constant(this.PathPoints[this.PathPoints.Count - 1].Y));
+      this.LineSegmentsX.Add(AffineLine.Constant(this.PathPoints[^1].X));
+      this.LineSegmentsY.Add(AffineLine.Constant(this.PathPoints[^1].Y));
     }
 
+    /// <summary>
+    /// Get the position such that the distance from the start of the path to the position along the path has supplied <paramref name="length"/>.
+    /// </summary>
+    /// <param name="length">Length along path to find position.</param>
+    /// <returns>The point <paramref name="length"/> along path.</returns>
     public Point GetPointAlongPath(double length)
     {
       int insertIndex = this.CumulativePathLengths.BinarySearch(length);
       insertIndex = insertIndex < 0 ? ~insertIndex : insertIndex;
-      return new Point()
-      {
-        X = this.LineSegmentsX[insertIndex].Evaluate(length),
-        Y = this.LineSegmentsY[insertIndex].Evaluate(length),
-      };
+      return new Point() { X = this.LineSegmentsX[insertIndex].Evaluate(length), Y = this.LineSegmentsY[insertIndex].Evaluate(length) };
     }
 
     /// <summary>
@@ -161,53 +164,49 @@ namespace BackEnd.Game
       private double A { get; init; }
 
       /// <summary>
-      /// Gets the contant value of line.
+      /// Gets the constant value of line.
       /// </summary>
       private double B { get; init; }
 
       /// <summary>
-      /// 
+      /// Initializes a new instance of the <see cref="AffineLine"/> class.
       /// </summary>
-      /// <param name="pathLength0"></param>
-      /// <param name="value0"></param>
-      /// <param name="pathLength1"></param>
-      /// <param name="value1"></param>
-
-      // TODO
       private AffineLine()
       {
       }
 
-      // TODO
+      /// <summary>
+      /// Initializes a new instance of the <see cref="AffineLine"/> class. Create the line from two points.
+      /// </summary>
+      /// <param name="pathLength0">Length along complete path of the first point.</param>
+      /// <param name="value0">Value of line at the first point.</param>
+      /// <param name="pathLength1">Length along complete path of the second point.</param>
+      /// <param name="value1">Value of line at the second point.</param>
       public AffineLine(double pathLength0, double value0, double pathLength1, double value1)
       {
         this.A = (value1 - value0) / (pathLength1 - pathLength0);
-        this.B = value0 - this.A * pathLength0; 
+        this.B = value0 - (this.A * pathLength0);
       }
 
-      public static AffineLine Constant(double contant)
+      public static AffineLine Constant(double constant)
       {
-        return new AffineLine()
-        {
-          A = 0,
-          B = contant,
-        };
+        return new AffineLine() { A = 0, B = constant };
       }
 
       /// <summary>
       /// Evaluate the value at a certain point.
-      /// <summary>
-      /// <param name="step">The step value to evaluate.</param>
+      /// </summary>
+      /// <param name="length">The input value to the line viewed as a function.</param>
       public double Evaluate(double length)
       {
-        return this.A * length + this.B;
+        return (this.A * length) + this.B;
       }
     }
 
     /// <summary>
     /// reference to a specific point on the game board.
     /// </summary>
-    public class Point 
+    public class Point
     {
       /// <summary>
       /// Gets the X coordinate of this <see cref="Point"/>.
@@ -219,12 +218,11 @@ namespace BackEnd.Game
       /// </summary>
       public double Y { get; init; }
 
-       /// <inheritdoc/>
+      /// <inheritdoc/>
       public override string ToString()
       {
         return $"Point {{ X: {this.X}, Y: {this.Y} }}";
       }
-
     }
 
     /// <summary>
