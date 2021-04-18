67fb042f215b983cdde8ae2c6e29f973084eda6a
le0-0 <leonard.schoyen@gmail.com>
2021-04-18 11:06:55 +0200

    Added Health components to enemies.

--- CoronaDefense/BackEnd/Game/Systems/PlaceEnemySystem.cs
+++ CoronaDefense/BackEnd/Game/Systems/PlaceEnemySystem.cs
@@ -15,24 +15,27 @@ namespace BackEnd.Game.Systems
     private readonly EcsWorld world = null;
 
     /// <summary>
-    /// Start fight round.
+    /// Place enemies.
     /// </summary>
     public void PlaceEnemies()
     {
-      // Add enemies
       double entryTime = 1d;
       for (int i = 1; i <= 10; i++)
       {
         entryTime += 0.5d;
 
         EcsEntity enemyEntity = this.world.NewEntity();
+        ref EnemyComponent enemy = ref enemyEntity.Get<EnemyComponent>();
+        enemy.PreviousImpactPosition = 0d;
+        enemy.PreviousImpactTime = entryTime;
+        ref HealthComponent enemyHealth = ref enemyEntity.Get<HealthComponent>();
+        enemyHealth.HealthPoints = 1;
         ref PathSpeedComponent enemySpeed = ref enemyEntity.Get<PathSpeedComponent>();
         enemySpeed.Speed = 1d;
         ref PathPositionComponent enemyPosition = ref enemyEntity.Get<PathPositionComponent>();
         enemyPosition.LengthTraveled = -enemySpeed.Speed * entryTime;
-        ref EnemyComponent enemy = ref enemyEntity.Get<EnemyComponent>();
-        enemy.PreviousImpactPosition = 0d;
-        enemy.PreviousImpactTime = entryTime;
+        ref ProjectedHealthComponent enemyProjectedHealth = ref enemyEntity.Get<ProjectedHealthComponent>();
+        enemyProjectedHealth.ProjectedHealthPoints = 1;
       }
     }
   }
