480420d49fd3510db662ccc9d8d04bfbe8694063
le0-0 <leonard.schoyen@gmail.com>
2021-04-23 17:02:50 +0200

    Removed old unused AssemblyDefinition.

--- CoronaDefense/BackEnd/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="AssemblyInfo.cs" company="NTNU: SWA group 1 (2021)">
-// Copyright (c) NTNU: SWA group 1 (2021). All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-#if DEBUG
-[assembly: InternalsVisibleTo("Backend.Tests")]
-#endif
